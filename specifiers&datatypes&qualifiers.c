495cc3d52b3e8135001e2146430c671029404244
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-03 20:05:27 +0200

    Create specifiers&datatypes&qualifiers.c

---
+++ specifiers&datatypes&qualifiers.c
@@ -0,0 +1,117 @@
+auto
+alignof
+alignas
+break
+bool
+case
+char
+char8_t
+char16_t
+char32_t
+const
+continue
+clock_t
+constexpr double
+consteval
+constinit
+constexpr
+default
+do
+double
+double _Complex
+double const
+decltype(auto)
+else
+enum
+extern
+float
+float _Complex
+for
+goto
+if
+inline
+int
+int8_t i8
+int8_t
+int16_t
+int32_t
+int64_t
+intmax_t
+int_least8_t
+int_least16_t
+int_least32_t
+int_least64_t
+int_least*_t
+int_fast8_t
+int_fast16_t
+int_fast32_t
+int_fast64_t
+int_fast*_t
+intptr_t
+long
+long double _Complex
+nullptr
+off_t
+ptrdiff_t
+printf
+register
+return
+restrict
+short
+size_t
+signed
+sizeof
+static
+struct
+switch
+std::nullptr_t
+std::byte
+static
+static_assert
+static inline
+ssize_t
+sig_atomic_t
+scanf
+type
+typedef
+typeof
+typeof_unqual
+time_t
+thread_local
+union
+unsigned
+uint
+uint8_t u8
+uint8_t
+uint16_t
+uint32_t
+uint64_t
+uint_least8_t
+uint_least16_t
+uint_least32_t
+uint_least64_t
+uint_fast8_t
+uint_fast16_t
+uint_fast32_t
+uint_fast64_t
+uintmax_t
+uintptr_t
+void
+volatile
+while
+wchar_t
+__attribute__
+_Atomic int a;
+_Alignas
+_Alignof
+_Atomic
+_Bool b;
+_BitInt
+_Decimal32
+_Decimal64
+_Decimal128
+_Generic
+_Noreturn
+_Static_assert
+__typeof__
+_Thread_local
