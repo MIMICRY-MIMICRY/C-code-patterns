845fe89e9d0cff1970c43facefb22b2b3c34a45f
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-07 00:12:00 +0200

    Add initial structure for variables and functions

---
+++ variables&functions&datatypes.c
@@ -0,0 +1,23 @@
+specifier variable = value
+data type variable = value
+operators variable = value
+qualificators variable = value
+
+specifier data type variable = value
+specifier qualificators variable = value
+specifier specifier data type variable = value
+data type data type variable = value
+qualificators data types variable = value
+
+specifier function()
+specifier data type function()
+specifier qualificators qualificators function()
+specifier qualificators qualificators data type function()
+
+data type function()
+data type data type function()
+
+qualificators function()
+qualificators data type function()
+qualificators qualificators function()
+qualificators qualificators data type function()
