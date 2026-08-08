816443876fe1683080e77dda4c451f537ce9ebc9
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-08 22:33:28 +0200

    Refactor variable and function specifiers in C

--- variables&functions&datatypes.c
+++ variables&functions&datatypes.c
@@ -1,23 +1,82 @@
 specifier variable = value
-data type variable = value
+modifier variable = value
 operators variable = value
-qualificators variable = value
+qualificator variable = value
 
-specifier data type variable = value
-specifier qualificators variable = value
-specifier specifier data type variable = value
-data type data type variable = value
-qualificators data types variable = value
+specifier variable = value
+specifier specifier variable = value
+specifier qualificator variable = value
+specifier qualificator qualificator variable = value
+specifier specifier variable = value
+specifier specifier qualificator qualificator variable = value
+specifier specifier qualificator variable = value
+
+qualificator variable = value
+qualificator specifier variable = value
+qualificator specifier specifier variable = value
+qualificator qualificator variable = value
+qualificator qualificator specifier variable = value
+qualificator qualificator specifier specifier variable = value
 
 specifier function()
-specifier data type function()
-specifier qualificators qualificators function()
-specifier qualificators qualificators data type function()
+specifier specifier function()
+specifier specifier specifier function()
+specifier qualificator function()
+specifier qualificator qualificator function()
+specifier specifier qualificator qualificator function()
 
-data type function()
-data type data type function()
+qualificator function()
+qualificator specifier function()
+qualificator specifier specifier function()
+qualificator specifier specifier specifier function()
+qualificator qualificator function()
+qualificator qualificator specifier function()
+qualificator qualificator specifier specifier function()
 
-qualificators function()
-qualificators data type function()
-qualificators qualificators function()
-qualificators qualificators data type function()
+// Specifier
+auto //
+bool //
+char //
+constexpr //
+double //
+extern //
+enum //
+float //
+int //
+inline // before function specifier
+long //
+register //
+static //
+struct //
+static_assert //
+thread_local //
+typedef //
+void //
+union //
+
+// Modifier
+short //
+signed //
+unsigned //
+
+// Qualificator
+const // 
+restrict //
+volatile //
+constexpr //
+
+// Operator
+alignas
+alignof
+sizeof //
+typeof //
+typeof_unqual //
+
+// Statements
+function(...){
+  body...
+}
+// Control flow blocks
+std::...::...::...
+// Identifiers
+number, value, alphabet 
