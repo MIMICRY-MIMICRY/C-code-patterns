ecca031484003d57b107c1892beb4dbf03b37cbf
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-09 20:28:28 +0200

    Add variable and constant specifiers documentation

---
+++ variables&constants.c
@@ -0,0 +1,27 @@
+specifier memory control _ specifier type _ constant type qualificator _ variable ->
+auto int ... float double long bool char ... signed & unsigned & short
+register int ... float double long bool char ... signed & unsigned & short
+static int ... float double long bool char ... signed & unsigned & short
+extern int ... float double long bool char ... signed & unsigned & short
+
+const int ... float double long bool char
+constexpr int ... float double long bool char
+volatile int ... float double long bool char
+restrict int ... float double long bool char
+enum int ... float double long bool char
+
+int ... float double long bool char
+
+signed & unsigned & short int ... float double long bool char
+
+typeof
+typeof_unqual
+typedef
+sizeof int ... float double long bool char ... signed & unsigned & short
+
+void int ... float double long bool char ... signed & unsigned & short
+
+inline
+
+nullptr
+enum
