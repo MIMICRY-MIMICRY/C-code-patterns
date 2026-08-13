769f952820681caea2e5d9ee244686aa81336c5d
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-13 23:40:17 +0200

    Add new variable and type specifiers

--- variables&constants.c
+++ variables&constants.c
@@ -1,19 +1,34 @@
 specifier memory control _ specifier type _ constant type qualificator _ variable ->
-auto int ... float double long bool char ... signed & unsigned & short
-register int ... float double long bool char ... signed & unsigned & short
-static int ... float double long bool char ... signed & unsigned & short
-extern int ... float double long bool char ... signed & unsigned & short
+auto int ... float double long bool char ... signed & unsigned & short + void & sizeof
+register int ... float double long bool char ... signed & unsigned & short + void & sizeof
+static int ... float double long bool char ... signed & unsigned & short + void & sizeof
+extern int ... float double long bool char ... signed & unsigned & short + void & sizeof
 
-const int ... float double long bool char ... signed & unsigned & short
-constexpr int ... float double long bool char ... signed & unsigned & short
-volatile int ... float double long bool char ... signed & unsigned & short
-restrict int ... float double long bool char ... signed & unsigned & short
-enum int ... float double long bool char ... signed & unsigned & short
+const int ... float double long bool char ... signed & unsigned & short + void & sizeof
+constexpr int ... float double long bool char ... signed & unsigned & short + void & sizeof
+constexpr double ...
+consteval ...
+constinit ...
+constexpr pointers ... *pointer = &adress of variable ... *pointer
+constexpr atomic types ...
+constexpr volatile types ...
+constexpr restrict pointers ... *pointer = &adress of variable ... *pointer
+volatile int ... float double long bool char ... signed & unsigned & short + void & sizeof
+restrict int ... float double long bool char ... signed & unsigned & short + void & sizeof
+enum int ... float double long bool char ... signed & unsigned & short + void & sizeof
 
-int ... float double long bool char
+int float double long bool char
+long double
+long double _Complex
+double long long
 
 signed & unsigned & short int ... float double long bool char
+signed char
 
+auto
+...
+const
+...
 typeof ... void int ... float double long bool char ... signed & unsigned & short
 typeof_unqual ... void int ... float double long bool char ... signed & unsigned & short
 typedef ... void int ... float double long bool char ... signed & unsigned & short
@@ -30,4 +45,34 @@ void & sizeof int signed & unsigned & short & double & long & long
 void & sizeof float signed & unsigned & short & double & long & long
 void & sizeof bool
 void & sizeof char short & long & long
+
+union auto const typeof int ... float double long bool char ... signed & unsigned & short
+
+struct ...
+
+static ...
+static_assert ...
+static inline ...
+
+size_t ...
+ssize_t ...
+
+auto ... const ... void int8_t i8
+auto ... const ... void int8_t ... intmax_t
+auto ... const ... void int_least8_t ...
+auto ... const ... void int_least*_t
+auto ... const ... void int_fast8_t ... int_fast*_t
+auto ... const ... void intptr_t
+auto ... const ... void nullptr *pointer = &adress of variable
+auto ... const ... void uchar_t
+auto ... const ... void uint
+auto ... const ... void uint8_t u8 ...
+auto ... const ... void uint_least8_t
+auto ... const ... void uint_fast8_t
+uintmax_t
+uintptr_t
+uint_least*_t
+uchar_t
+
+typedef typeof((int*)nullptr - (int*)nullptr) ptrdiff_t;
 
