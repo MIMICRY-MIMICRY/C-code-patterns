2d7b61feb93ff77c773e06e14030170ae90c8560
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-13 09:59:36 +0200

    Refactor preprocessor macros in #preprocessor.c

--- #preprocessor.c
+++ #preprocessor.c
@@ -7,7 +7,17 @@
 #define NAME FUNC(x) ((x)*(x))
 #define NAME FUNC(a, b) ((a) > (b) ? (a) : (b))
 #define NAME VARIABLE/FUNC(..., ...) ...##... / #...
-#define NAME(...) NAME(...)
+#define NAME func(...) NAME func(...)
+#define NAME variable NAME func(variable)
+#define NAME variable value
+#define	CCP_VSIZE 16
+#define	CCP_VMASK		((unsigned int)((1 << CCP_VSIZE) - 1))
+#define	CCP_VERSION(v, r)	((unsigned int)((v << CCP_VSIZE) \
+					       | (r & CCP_VMASK)))
+// Makro zwracające wskaźnik do funkcji
+#define URUCHOM_DLA(x) funkcja_docelowa
+// Wywołanie w kodzie wygląda tak:
+URUCHOM_DLA(x)(y); 
 #define NAME VARIABLE/FUNC(..., ...) printf(..., __VA_ARGS__)
 #define NAME VARIABLE/FUNC(..., ...) kprintf("... " ... "...", __FILE__, __LINE__, ##__VA_ARGS__)
 #define NAME VARIABLE/FUNC(...) fprintf(..., __VA_ARGS__)
