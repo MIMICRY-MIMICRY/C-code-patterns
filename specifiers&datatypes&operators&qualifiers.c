08305ba276875af083c81326fd95921d36c92575
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-14 10:56:48 +0200

    Add atomic types and memory order constants

--- specifiers&datatypes&operators&qualifiers.c
+++ specifiers&datatypes&operators&qualifiers.c
@@ -147,3 +147,59 @@ _Thread_local // compiler gcc specific extensions
 %lu // unsigned int or unsigned long
 %li or %lid // Long long
 %llu // unsigned long long
+atomic_bool	_Atomic _Bool
+atomic_char	_Atomic char
+atomic_schar	_Atomic signed char
+atomic_uchar	_Atomic unsigned char
+atomic_short	_Atomic short
+atomic_ushort	_Atomic unsigned short
+atomic_int	_Atomic int
+atomic_uint	_Atomic unsigned int
+atomic_long	_Atomic long
+atomic_ulong	_Atomic unsigned long
+atomic_llong	_Atomic long long
+atomic_ullong	_Atomic unsigned long long
+atomic_char16_t	_Atomic char16_t
+atomic_char32_t	_Atomic char32_t
+atomic_wchar_t	_Atomic wchar_t
+atomic_int_least8_t	_Atomic int_least8_t
+atomic_uint_least8_t	_Atomic uint_least8_t
+atomic_int_least16_t	_Atomic int_least16_t
+atomic_uint_least16_t	_Atomic uint_least16_t
+atomic_int_least32_t	_Atomic int_least32_t
+atomic_uint_least32_t	_Atomic uint_least32_t
+atomic_int_least64_t	_Atomic int_least64_t
+atomic_uint_least64_t	_Atomic uint_least64_t
+atomic_int_fast8_t	_Atomic int_fast8_t
+atomic_uint_fast8_t	_Atomic uint_fast8_t
+atomic_int_fast16_t	_Atomic int_fast16_t
+atomic_uint_fast16_t	_Atomic uint_fast16_t
+atomic_int_fast32_t	_Atomic int_fast32_t
+atomic_uint_fast32_t	_Atomic uint_fast32_t
+atomic_int_fast64_t	_Atomic int_fast64_t
+atomic_uint_fast64_t	_Atomic uint_fast64_t
+atomic_intptr_t	_Atomic intptr_t
+atomic_uintptr_t	_Atomic uintptr_t
+atomic_size_t	_Atomic size_t
+atomic_ptrdiff_t	_Atomic ptrdiff_t
+atomic_intmax_t	_Atomic intmax_t
+atomic_uintmax_t	_Atomic uintmax_t
+atomic_bool	ATOMIC_BOOL_LOCK_FREE
+atomic_char	ATOMIC_CHAR_LOCK_FREE
+atomic_char16_t	ATOMIC_CHAR16_T_LOCK_FREE
+atomic_char32_t	ATOMIC_CHAR32_T_LOCK_FREE
+atomic_wchar_t	ATOMIC_WCHAR_T_LOCK_FREE
+atomic_short	ATOMIC_SHORT_LOCK_FREE
+atomic_int	ATOMIC_INT_LOCK_FREE
+atomic_long	ATOMIC_LONG_LOCK_FREE
+atomic_llong	ATOMIC_LLONG_LOCK_FREE
+atomic_intptr_t	ATOMIC_POINTER_LOCK_FREE
+memory_order_seq_cst	Sequential Consistency
+memory_order_acq_rel	Acquire/Release
+memory_order_release	Release
+memory_order_acquire	Acquire
+memory_order_consume	Consume
+memory_order_relaxed	Relaxed
+0	Never lock-free.
+1	Sometimes lock-free.
+2	Always lock-free.
