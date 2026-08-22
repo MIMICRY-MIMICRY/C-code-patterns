48a6acc45e10a00c73dcf7f59db4113da405abe8
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-22 18:35:03 +0200

    Add initial implementation of pointer and address usage

---
+++ Pointers&adresses.c
@@ -0,0 +1,10 @@
+*pointer
+*pointer=&adress
+&adress
+&adress->variable
+variable->variable
+*pointer=func(variable)
+*pointer=variable->variable
+variable->variable->variable
+variable->variable->func(variable)
+*pointer=&adress->variable
