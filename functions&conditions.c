bf887da9d483530ad01195353c2d419ce826b1d2
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-29 00:20:48 +0200

    Add various function and type definitions in C

--- functions&conditions.c
+++ functions&conditions.c
@@ -1,3 +1,57 @@
-blueprint
+blueprint;
 bool has_ns_capability(struct task_struct *t,
-		       struct user_namespace *ns, int cap)
+		       struct user_namespace *ns, int cap);
+
+constexpr function() {};
+
+constexpr double function() {};
+
+constexpr volatile function() {};
+
+constexpr restrict function() {};
+
+volatile function() {};
+
+volatile struct {double variable; char line;}; 
+function() {};
+
+volatile const function();
+
+volatile int function();
+
+enum Day {monday};
+int function(void){
+	enum Day today = monday;
+};
+
+typeof function() {};
+
+typeof_unqual function() {};
+
+typedef unsigned long ulong;
+typedef struct {double variable; double variable;}Point;
+int function(void) {};
+typedef struct toto toto;
+toto const* toto_zero(void) [[unsequenced]];
+
+static void function(variable);
+
+struct objects {char name; int val; float digit;}; 
+void function(){
+	struct objects object = {"",0,0.0};
+};
+
+static_assert function() {};
+
+static inline function() {};
+
+inline function(variable) {};
+
+union functions 
+{
+	double function;
+	bool function;
+	void fucntion;
+} function();
+
+void function(void){};
