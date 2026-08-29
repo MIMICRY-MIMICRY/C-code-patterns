e0eabf72c1b5935cc64007cb0774d3adb073078e
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-29 16:20:58 +0200

    Fix pointer assignment and type constraint issues
    Correct pointer assignments and address violations.

--- Pointers&adresses.c
+++ Pointers&adresses.c
@@ -9,3 +9,9 @@ variable->variable->variable
 variable->variable->func(variable)
 *pointer=&adress->variable
 dp = &(s2->d[0]);
+ncs = cs; // valid
+cs = ncs; // violates modifiable lvalue constraint for =
+pi = &ncs.mem; // valid
+pi = &cs.mem; // violates type constraints for =
+pci = &cs.mem; // valid
+pi = a[0]; // invalid: a[0] has type "const int *"
