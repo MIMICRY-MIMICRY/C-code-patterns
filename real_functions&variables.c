30adc6257eb4dab6e096212a2d17a07b39989f03
MIMICRY-MIMICRY <maiklf50@gmail.com>
2026-08-30 18:49:46 +0200

    Add function declarations for multiple data types
    Added various function declarations with different types.

--- real_functions&variables.c
+++
@@ -1,7 +0,0 @@
-DEFINE_FREE(fput_sync, struct file *, if (!IS_ERR_OR_NULL(_T)) __fput_sync(_T))
-static int acct_on(const char __user *name)
-
-struct file *_T = *(struct file **)p;
-
-CLASS(filename, pathname)(name);
-
---
+++ real_functions&variables.h
@@ -0,0 +1,32 @@
+DEFINE_FREE(fput_sync, struct file *, if (!IS_ERR_OR_NULL(_T)) __fput_sync(_T))
+static int acct_on(const char __user *name)
+
+struct file *_T = *(struct file **)p;
+
+CLASS(filename, pathname)(name);
+
+int  long  double  bool  char  float  signed  unsigned  short  sizeof 
+struct  enum  typedef  typeof  typeof_unqual  union
+_Atomic _BitInt _Complex  _Decimal128  _Decimal32  _Decimal64  _Generic  _Imaginary  inline  _Noreturn
+auto  register  extern  restrict  thread_local  nullptr  const  constexpr  volatile  static  static_assert
+void  
+
+signed int long long FUNCTION(signed int variable, auto volatile variable, struct name variable, struct name *pointer) {};
+signed int short FUNCTION(void function(), variable) {};
+signed long long FUNCTION() {};
+signed double long FUNCTION() {};
+signed bool FUNCTION() {};
+signed char FUNCTION() {};
+signed float FUNCTION() {};
+signed short FUNCTION() {};
+unsigned int long long FUNCTION() {};
+unsigned int short FUNCTION() {};
+unsigned long long FUNCTION() {};
+unsigned double long FUNCTION() {};
+unsigned bool FUNCTION() {};
+unsigned char FUNCTION() {};
+unsigned float FUNCTION() {};
+unsigned short FUNCTION() {};
+
+void FUNCTION() {};
+static void FUNCTION(struct name *work) {};
